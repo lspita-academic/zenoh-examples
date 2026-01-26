e2a1c637bd0f6aa313d7a4ddfbb68bbd91f31ca8
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-01-26 19:14:43 +0000

    formatting

--- crates/hello-zenoh/src/bin/pub.rs
+++ crates/hello-zenoh/src/bin/pub.rs
@@ -6,14 +6,18 @@ use hello_zenoh::{KEY, get_config};
 #[tokio::main]
 async fn main() {
     println!("Starting publisher");
+
     let config = get_config();
     let session = zenoh::open(config).await.unwrap();
     println!("Session opened successfully");
+
     let info = session.info();
     println!("Session ID: {}", info.zid().await);
     println!("Peers: {:?}", info.peers_zid().await.collect::<Vec<_>>());
+
     let publisher = session.declare_publisher(KEY).await.unwrap();
     println!("Publisher for {} created successfully", KEY);
+
     sleep(Duration::from_millis(1000)).await; // wait for subscriber to start
     println!("Starting publishing");
     for i in 0..10 {
--- crates/hello-zenoh/src/bin/sub.rs
+++ crates/hello-zenoh/src/bin/sub.rs
@@ -3,14 +3,18 @@ use hello_zenoh::{KEY, get_config};
 #[tokio::main]
 async fn main() {
     println!("Starting subscriber");
+
     let config = get_config();
     let session = zenoh::open(config).await.unwrap();
     println!("Session opened successfully");
+
     let info = session.info();
     println!("Session ID: {}", info.zid().await);
     println!("Peers: {:?}", info.peers_zid().await.collect::<Vec<_>>());
+
     let subscriber = session.declare_subscriber(KEY).await.unwrap();
     println!("Subscriber for {} created successfully", KEY);
+
     println!("Starting read loop");
     while let Ok(sample) = subscriber.recv_async().await {
         let payload = sample
