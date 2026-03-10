71160eae25965a85c9bfee24b4ca8bb3b20a68de
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-03-11 00:32:35 +0100

    embassy hello world

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-fn main() {
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+use esp_idf_svc::log::EspLogger;
+
+#[embassy_executor::task]
+async fn hello_world() {
+    loop {
+        log::info!("Hello, world!");
+        Timer::after_secs(1).await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
 
     // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
-
-    log::info!("Hello, world!");
+    EspLogger::initialize_default();
+    let _ = spawner.spawn(hello_world());
 }
