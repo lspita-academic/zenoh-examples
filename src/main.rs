83eeec33494353eac51bbe2e77d32ea1e7c3cd5d
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-03-12 08:11:34 +0100

    zenoh: logging config mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ffi::CStr;
+
 use embassy_executor::Spawner;
 use embassy_time::Timer;
 use esp_idf_svc::log::EspLogger;
@@ -24,5 +26,21 @@ async fn main(spawner: Spawner) {
 
     // Bind the log crate to the ESP Logging facilities
     EspLogger::initialize_default();
+
+    let mut zenoh_config = zenoh_pico::z_owned_config_t::default();
+    unsafe {
+        let ok = zenoh_pico::z_config_default(&mut zenoh_config);
+        assert!(ok == 0, "Cannot create default zenoh config");
+        let config_mode_ptr = zenoh_pico::zp_config_get(
+            &raw const zenoh_config._val,
+            zenoh_pico::Z_CONFIG_MODE_KEY as u8,
+        );
+        if config_mode_ptr == 0x0 as *const u8 {
+            panic!("Config mode not found!")
+        }
+        let config_mode = CStr::from_ptr(config_mode_ptr);
+        log::info!("Config mode: {:?}", config_mode);
+    }
+
     let _ = spawner.spawn(hello_world());
 }
